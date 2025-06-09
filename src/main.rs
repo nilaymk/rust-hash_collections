9daae841b3c015b7489ad55c02b792e7d9297202
Nilay Kothari <nilaymk@outlook.com>
2025-06-09 10:57:51 +0200

    UNTESTED: initial implementation of a basic hash_map

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,120 @@
-#![feature(adt_const_params, generic_const_exprs)] 
+#![feature(adt_const_params, )] 
+#![feature(const_type_id)]
+#![feature(generic_const_exprs)]
+#![feature(core_intrinsics)]
+#![feature(inherent_associated_types)]
+
 
 use const_primes::{is_prime};
 use const_guards::guard;
+use std::any::{Any};
+use std::hash::{DefaultHasher, Hash, Hasher};
+use std::mem;
 
-type KeyType = String;
-type ValType = i64;
-type CapType = u16;
-
-struct Entry {
-    key: KeyType,
-    value: ValType,
-    next: CapType,
-    previous: CapType
+struct Entry <K, V> {
+    key: K,
+    value: V,
+    next: i64,
+    prev: i64
 }
 
-struct FixedSizeHashMap <const C: CapType>  {
-    _data: Vec<Entry>,
-    _size: CapType,
+impl <K, V> Entry <K, V> {
+    fn new(key: K, value: V) -> Entry<K, V> {
+        Entry {
+            key: key,
+            value: value,
+            next: -1,
+            prev: -1,
+        }
+    }
 }
+
+struct FixedSizeHashMap <K, V, const C: usize>  {
+    _data: Vec<Option<Entry<K, V>>>,
+    _size: usize,
+}
+
+const fn is_integral_or_string<T: ?Sized + Any>() -> bool {
+    core::intrinsics::type_id::<T>() == core::intrinsics::type_id::<String>()
+    || core::intrinsics::type_id::<T>() == core::intrinsics::type_id::<i32>()
+    || core::intrinsics::type_id::<T>() == core::intrinsics::type_id::<i64>()
+    || core::intrinsics::type_id::<T>() == core::intrinsics::type_id::<i128>()
+    || core::intrinsics::type_id::<T>() == core::intrinsics::type_id::<u32>()
+    || core::intrinsics::type_id::<T>() == core::intrinsics::type_id::<u64>()
+    || core::intrinsics::type_id::<T>() == core::intrinsics::type_id::<u128>()
+}
+
 
 #[guard(is_prime(C as u64))]
-#[guard(C < 1000)]
-impl <const C: CapType> FixedSizeHashMap <C>  {
-    const CAPACITY: CapType = C;
-    const CHECK: bool = is_prime(C as u64);
+#[guard(C <= 25013)]
+impl <K: Hash, V, const C: usize> FixedSizeHashMap <K, V, C>  {
+    const CAPACITY: usize = C;
 
-    pub fn new() -> FixedSizeHashMap<C> {        
-        FixedSizeHashMap { _data: Vec::with_capacity(Self::CAPACITY as usize), _size: 0 }
+    fn calculate_index(key: &K) -> u64 {
+        let mut s = DefaultHasher::new();
+        key.hash(&mut s);
+        s.finish() % Self::CAPACITY as u64
     }
 
-    pub fn insert(&mut self, key: KeyType, v: ValType ) -> Option<ValType> {
-        None
-
+    pub fn new() -> FixedSizeHashMap<K, V, C> {
+        FixedSizeHashMap {
+            _data: Vec::with_capacity(Self::CAPACITY as usize),
+            _size: 0
+        }
     }
 
-    pub fn exists(&self, key: KeyType) -> bool {    
-        false
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        let i = Self::calculate_index(&key) as usize;
+        match self._data[i].as_mut() {
+            Some(entry) => {
+                let old_val = mem::replace(&mut entry.value, value);
+                Some(old_val)
+            }
+            None => {
+                self._data[i] = Some(Entry::new(key, value));
+                self._size += 1;
+                None
+            }
+        }
     }
 
-    pub fn get(&self, key: KeyType) -> Option<&ValType> {
-        None
+    pub fn exists(&self, key: &K) -> bool {
+        let i = Self::calculate_index(key) as usize;
+        self._data[i].is_some()
     }
 
-    pub const fn capacity(&self) -> CapType {
+    pub fn get(&self, key: &K) -> Option<&V> {
+        let i = Self::calculate_index(&key) as usize;
+        match self._data[i].as_ref() {
+            Some(data) => Some(&data.value),
+            None => None
+        }
+    }
+
+    pub fn remove(&mut self, key: &K) -> Option<V> {
+        let i = Self::calculate_index(&key) as usize;
+        let old_entry = mem::replace(&mut self._data[i], None);
+        match old_entry {
+            None => None,
+            Some(key_val) => {
+                self._size -= 1;
+                Some(key_val.value)
+            } 
+        }
+    } 
+
+    pub const fn capacity(&self) -> usize {
         Self::CAPACITY
     }
     
-    const fn cap() -> CapType {
-        Self::CAPACITY
-    }
-
-    pub fn size(&self) -> CapType {
+    pub fn size(&self) -> usize {
         self._size
     }
 }
 
-fn main() {
-    let fmap: FixedSizeHashMap<3> = FixedSizeHashMap::new();
+type MyMap = FixedSizeHashMap<String, String, 97>;
 
+fn main() {
+    let fmap: MyMap = MyMap::new();
     println!("Hello, world!");
 }
